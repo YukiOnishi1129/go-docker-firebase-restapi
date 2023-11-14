53fc51702b6a78ff80c5b57c773e3b7d499b3181
yukionishi1129 <yuki.onishi1129@gmail.com>
2023-11-15 01:53:04 +0900

    firebase setup

--- Makefile
+++ Makefile
@@ -10,5 +10,6 @@ backend-ssh:
 
 # ローカル開発用
 # go library install
+## 複数のライブラリを指定する場合は、name="xxx yyy" のように""で囲んで実行すること
 go-add-library:
 	docker exec -it ${BACKEND_CONTAINER_NAME} sh -c "go get ${name}"
--- app/server.go
+++ app/server.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 	"log"
 	"net/http"
 )
 
 func main() {
 	log.Printf("connect to http://localhost:%s/ for rest api", "4000")
+
+	opt := option.WithCredentialsFile("firebase/serviceAccountKey.json")
+	_, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		//return nil, fmt.Errorf("error initializing app: %v", err)
+		log.Printf("error initializing app: %v", err)
+	}
+	log.Printf("ok initializing app")
+
 	http.ListenAndServe(":4000", nil)
 }
